e3fb818c0664aaa34af4f060c56f6f29507c321a
awni <awni.hannun@gmail.com>
2022-02-09 09:55:55 -0800

    minor setup

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, Extension
 import sys
-from torch.utils import cpp_extension
+from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 if sys.platform == "darwin":
     args = ["-DAPPLE"]
 else:
     args = ["-fopenmp"]
 
+args.append("-std=c++14")
 
-ext = Extension(
+ext = CppExtension(
         name='transducer_cpp',
         sources=['transducer.cpp'],
-        include_dirs=cpp_extension.include_paths(),
-        extra_compile_args=args,
-        language='c++')
+        extra_compile_args=args)
 
 setup(name='transducer_cpp',
+      version="0.0",
       ext_modules=[ext],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
+      cmdclass={'build_ext': BuildExtension})
 
