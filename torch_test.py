0abc37e9a2cf7222d159290d0dc9871ed105e4de
awni <awni.hannun@gmail.com>
2022-02-11 09:25:24 -0800

    torch loss with working python binding

---
+++ torch_test.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+import unittest
+import numpy as np
+import torch
+import torch.nn as nn
+
+from transducer import TransducerLoss
+
+def apply_transducer(emissions, predictions, labels, use_cuda=False):
+    emissions = torch.tensor(
+        emissions, dtype=torch.float32, requires_grad=True)
+    predictions = torch.tensor(
+        predictions, dtype=torch.float32, requires_grad=True)
+    if use_cuda:
+        emissions = emissions.cuda()
+        predictions = predictions.cuda()
+
+    lengths = [emissions.shape[1]] * emissions.shape[0]
+    label_lengths = [len(l) for l in labels]
+    labels = [l for label in labels for l in label]
+    labels = torch.IntTensor(labels)
+    lengths = torch.IntTensor(lengths)
+    label_lengths = torch.IntTensor(label_lengths)
+
+    costs = TransducerLoss(
+        emissions, predictions, labels, lengths, label_lengths)
+    torch.sum(costs).backward()
+    return costs.cpu(), emissions.grad.cpu(), predictions.grad.cpu()
+
+class TestTransducerLoss(unittest.TestCase):
+
+    def small_test():
+        emissions = [[
+            [0.1, 0.6, 0.1, 0.1, 0.1],
+            [0.1, 0.1, 0.2, 0.1, 0.1]]]
+        predictions = [[
+              [0.1, 0.6, 0.1, 0.1, 0.1],
+              [0.1, 0.1, 0.6, 0.1, 0.1],
+              [0.1, 0.1, 0.2, 0.8, 0.1]]]
+        labels = [[1, 2]]
+
+        cost, egrads, pgrads = apply_transducer(emissions, predictions, labels)
+        expected_cost = torch.tensor([4.843925], dtype=torch.float32)
+        expected_egrads = torch.tensor(
+            [[[-0.6884234547615051, -0.05528555065393448, 0.0728120282292366, 0.3593204915523529, 0.3115764856338501],
+              [-0.6753658056259155, 0.08346927165985107, -0.21995842456817627, 0.48722079396247864, 0.3246341943740845]]],
+            dtype=torch.float32)
+        expected_pgrads = torch.tensor(
+            [[[-0.07572315633296967, -0.5175057649612427, 0.2010551244020462, 0.19608692824840546, 0.19608692824840546],
+              [-0.1768123358488083, 0.30765989422798157, -0.5961406826972961, 0.23264653980731964, 0.23264653980731964],
+              [-1.1112537384033203, 0.2380295693874359, 0.24793916940689087, 0.41780781745910645, 0.20747721195220947]]],
+            dtype=torch.float32)
+
+        self.assertTrue(torch.allclose(cost, expected_cost))
+        self.assertTrue(torch.allclose(egrads, expected_egrads))
+        self.assertTrue(torch.allclose(pgrads, expected_pgrads))
+
+    def big_test():
+
+        # batch_size x T x alphabet_size
+        emissions = [
+            [[0.8764081559029704, 0.8114401931890338, 0.6508828493896047],
+             [0.6831969720272136, 0.794939425350507, 0.4771495462110181],
+             [0.07800002444603382, 0.007794919225017516, 0.9478301043860103],
+             [0.49619506263326396, 0.7345710606552497, 0.7741700701082916]],
+
+            [[0.7084607475161292, 0.9860726712179101, 0.7902338818255793],
+             [0.7691063457590045, 0.5448267745331934, 0.22524027048482376],
+             [0.2291088288701465, 0.7524300104847589, 0.7273355024795244],
+             [0.33155408518920104, 0.8068789770558062, 0.6188633401048291]]]
+
+        # batch_size x U x alphabet_size
+        predictions = [
+            [[0.6223532638505989, 0.3002940148933876, 0.7404674033386307],
+             [0.01823584315362603, 0.034963374948701054, 0.34892745941957193],
+             [0.5718051448658747, 0.28205981250440926, 0.7283146324887043]],
+
+            [[0.7755842032974967, 0.5521231124815825, 0.8577769985498179],
+             [0.42450076602299125, 0.9417870425381804, 0.0072059916072961805],
+             [0.37187505831579304, 0.960974111779922, 0.04504344671276461]]]
+
+        labels = [[1, 2],
+                  [1, 1]]
+
+        costs, egrads, pgrads = apply_transducer(emissions, predictions, labels)
+
+        expected_costs = torch.tensor(
+            [4.718404769897461, 4.803375244140625], dtype=torch.float32)
+        expected_egrads = torch.tensor(
+            [[[-0.4596531093120575, 0.041041433811187744, 0.4186115860939026],
+              [-0.4770655333995819, 0.13196370005607605, 0.34510183334350586],
+              [-0.6760067939758301, 0.09430177509784698, 0.5817050337791443],
+              [-0.5915795564651489, 0.29016029834747314, 0.3014192581176758]],
+
+            [[-0.5917761325836182, 0.15546470880508423, 0.4363115429878235],
+              [-0.4406549036502838, 0.14964917302131653, 0.2910056710243225],
+              [-0.6741735935211182, 0.23876483738422394, 0.4354088008403778],
+              [-0.6422789096832275, 0.2854732275009155, 0.356805682182312]]],
+            dtype=torch.float32)
+
+        expected_pgrads = torch.tensor(
+            [[[-0.3262518346309662, -0.46784698963165283, 0.7940987944602966],
+              [-0.429027259349823, 0.5465580821037292, -0.11753084510564804],
+              [-1.4490258693695068, 0.4787561297416687, 0.9702697992324829]],
+
+             [[-0.5165280699729919, -0.28539586067199707, 0.8019239902496338],
+              [-0.4294244050979614, 0.07082393765449524, 0.3586004972457886],
+              [-1.4029310941696167, 1.0439238548278809, 0.35900723934173584]]],
+             dtype=torch.float32)
+
+        self.assertTrue(torch.allclose(costs, expected_costs))
+        self.assertTrue(torch.allclose(egrads, expected_egrads))
+        self.assertTrue(torch.allclose(pgrads, expected_pgrads))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ transducer/torch_binding.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+import torch
+from ._transducer import *
+
+class Transducer(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, emissions, predictions, labels, lengths, label_lengths, blank=0):
+        """
+        Computes the Transducer cost for a minibatch of examples.
+
+        Arguments:
+            emissions (FloatTensor): Unnormalized emission scores should
+                be of shape (minibatch, input length, vocab size).
+            predictions (FloatTensor): Unnormalized prediction scores should
+                be of shape (minibatch, output length + 1, vocab size).
+            labels (IntTensor): 1D tensor of labels for each example
+                consecutively.
+            lengths (IntTensor): 1D tensor of number actviation time-steps
+                for each example.
+            label_lengths (IntTensor): 1D tensor of label lengths for
+                each example.
+            blank (int, optional): Integer id of blank label (default is 0).
+
+        Returns:
+            costs (FloatTensor): .
+        """
+        is_cuda = emissions.is_cuda
+        device = emissions.device
+        dtype = emissions.dtype
+
+        B, T, V = emissions.shape
+        U = predictions.shape[1]
+        certify_inputs(emissions, predictions, labels, lengths, label_lengths)
+        costs = torch.tensor(B, device=emissions.device, dtype=dtype)
+        alphas = torch.tensor((B, T, U), device=device, dtype=dtype)
+        log_norms = torch.tensor((B, T, U), device=device, dtype=dtype)
+        _transducer.forward(
+            emissions.data_ptr,
+            predictions.data_ptr,
+            costs.data_ptr,
+            alphas.data_ptr,
+            log_norms.data_ptr,
+            labels.data_ptr,
+            lengths.data_ptr,
+            label_lengths.data_ptr,
+            B, T, U, V, blank, is_cuda)
+        ctx.save_for_backward(alphas, log_norms)
+        ctx.sizes = (B, T, U, V)
+        return costs
+
+    @staticmethod
+    def backward(ctx, cost):
+        is_cuda = cost.is_cuda
+        device = cost.device
+        dtype = cost.dtype
+        B, T, U, V = ctx.sizes
+        egrads = torch.tensor((B, T, V), device=device, dtype=dtype)
+        pgrads = torch.tensor((B, U, V), device=device, dtype=dtype)
+        alphas = ctx.saved_tensors[0]
+        log_norms = ctx.saved_tensors[1]
+        _transducer.backward(
+            emissions.data_ptr,
+            predictions.data_ptr,
+            egrads.data_ptr,
+            pgrads.data_ptr,
+            alphas.data_ptr,
+            log_norms.data_ptr,
+            labels.data_ptr,
+            lengths.data_ptr,
+            label_lengths.data_ptr,
+            B, T, U, V, blank, is_cuda)
+        return egrads, pgrads, None, None, None, None
+
+
+class TransducerLoss(torch.nn.Module):
+    def __init__(self, blank=0):
+        super(TransducerLoss, self).__init__()
+        self.blank = blank
+
+    def forward(self, emissions, predictions, labels, lengths, label_lengths):
+        return Transducer.apply(
+            emissions, predictions, labels, lenghts, label_lengths, self.blank)
+
+
+def check_type(var, t, name):
+    if var.dtype is not t:
+        raise TypeError("{} must be {}".format(name, t))
+
+
+def check_contiguous(var, name):
+    if not var.is_contiguous():
+        raise ValueError("{} must be contiguous".format(name))
+
+
+def check_dim(var, dim, name):
+    if len(var.shape) != dim:
+        raise ValueError("{} must be {}D".format(name, dim))
+
+
+def certify_inputs(emissions, predictions, labels, lengths, label_lengths):
+    check_type(emissions, torch.float32, "emissions")
+    check_type(predictions, torch.float32, "predictions")
+    check_type(labels, torch.int32, "labels")
+    check_type(label_lengths, torch.int32, "label_lengths")
+    check_type(lengths, torch.int32, "lengths")
+    check_contiguous(labels, "labels")
+    check_contiguous(label_lengths, "label_lengths")
+    check_contiguous(lengths, "lengths")
+
+    if emissions.shape[2] != predictions.shape[2]:
+        raise ValueError("vocab size mismatch.")
+
+    if lengths.shape[0] != emissions.shape[0]:
+        raise ValueError("must have a length per example.")
+    if label_lengths.shape[0] != emissions.shape[0]:
+        raise ValueError("must have a label length per example.")
+
+    check_dim(emissions, 3, "emissions")
+    check_dim(predictions, 3, "predictions")
+    check_dim(labels, 1, "labels")
+    check_dim(lengths, 1, "lenghts")
+    check_dim(label_lengths, 1, "label_lenghts")
+    max_T = torch.max(lengths)
+    max_U = torch.max(label_lengths)
+    T = emissions.shape[1]
+    U = predictions.shape[1]
+    if T != max_T:
+        raise ValueError("Input length mismatch")
+    if U != max_U + 1:
+        raise ValueError("Output length mismatch")
