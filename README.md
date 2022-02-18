f544e63eb7af145e755d4189ee524d091fb93d06
Awni Hannun <awni.hannun@gmail.com>
2022-02-18 23:19:25 +0000

    docs

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # transducer
 
-A Fast Sequence Transducer Implementation with python bindings and a PyTorch
-extension. The corresponding publication is [Sequence Transduction with
-Recurrent Neural Networks](https://arxiv.org/abs/1211.3711).
+A fast RNN-Transducer implementation on the CPU and GPU (CUDA) with python
+bindings and a PyTorch extension. The RNN-T loss function was published in
+[Sequence Transduction with Recurrent Neural Networks](https://arxiv.org/abs/1211.3711).
 
 The code has been tested with Python 3.9. and PyTorch 1.9.
 
--- transducer/torch_binding.py
+++ transducer/torch_binding.py
@@ -5,135 +5,163 @@ from . import _transducer
 
 class Transducer(torch.autograd.Function):
 
-    @staticmethod
-    def forward(ctx, emissions, predictions, labels, input_lengths, label_lengths, blank=0):
-        """
-        Computes the Transducer cost for a minibatch of examples.
+  @staticmethod
+  def forward(ctx, emissions, predictions, labels, input_lengths, label_lengths, blank=0):
+    """
+    Computes the Transducer cost for a minibatch of examples.
 
-        Arguments:
-            emissions (FloatTensor): Unnormalized emission scores should
-                be of shape (minibatch, input length, vocab size).
-            predictions (FloatTensor): Unnormalized prediction scores should
-                be of shape (minibatch, output length + 1, vocab size).
-            labels (IntTensor): 2D tensor of labels for each example of shape
-                (minibatch, output length), shorter labels are padded to the
-                length of the longest label.
-            input_lengths (IntTensor): 1D tensor of number actviation time-steps
-                for each example.
-            label_lengths (IntTensor): 1D tensor of label lengths for
-                each example.
-            blank (int, optional): Integer id of blank label (default is 0).
+    Arguments:
+        emissions (FloatTensor): Unnormalized emission scores should
+            be of shape (minibatch, input length, vocab size).
+        predictions (FloatTensor): Unnormalized prediction scores should
+            be of shape (minibatch, output length + 1, vocab size).
+        labels (IntTensor): 2D tensor of labels for each example of shape
+            (minibatch, output length), shorter labels are padded to the
+            length of the longest label.
+        input_lengths (IntTensor): 1D tensor of number input time-steps
+            for each example.
+        label_lengths (IntTensor): 1D tensor of label lengths for
+            each example.
+        blank (int, optional): Integer id of blank label (default is 0).
 
-        Returns:
-            costs (FloatTensor): .
-        """
-        is_cuda = emissions.is_cuda
-        device = emissions.device
-        dtype = emissions.dtype
+    Returns:
+        costs (FloatTensor): .
+    """
+    is_cuda = emissions.is_cuda
+    device = emissions.device
+    dtype = emissions.dtype
 
-        B, T, V = emissions.shape
-        U = predictions.shape[1]
-        certify_inputs(emissions, predictions, labels, input_lengths, label_lengths)
-        costs = torch.empty(size=(B,), device=emissions.device, dtype=dtype)
-        alphas = torch.empty(size=(B, T, U), device=device, dtype=dtype)
-        log_norms = torch.empty(size=(B, T, U), device=device, dtype=dtype)
-        _transducer.forward(
-            emissions.data_ptr(),
-            predictions.data_ptr(),
-            costs.data_ptr(),
-            alphas.data_ptr(),
-            log_norms.data_ptr(),
-            labels.data_ptr(),
-            input_lengths.data_ptr(),
-            label_lengths.data_ptr(),
-            B, T, U, V, blank, is_cuda)
-        ctx.save_for_backward(
-            emissions, predictions, alphas, log_norms,
-            labels, input_lengths, label_lengths)
-        ctx.blank = blank
-        return costs
+    B, T, V = emissions.shape
+    U = predictions.shape[1]
+    certify_inputs(emissions, predictions, labels, input_lengths, label_lengths)
+    costs = torch.empty(size=(B,), device=emissions.device, dtype=dtype)
+    alphas = torch.empty(size=(B, T, U), device=device, dtype=dtype)
+    log_norms = torch.empty(size=(B, T, U), device=device, dtype=dtype)
+    _transducer.forward(
+        emissions.data_ptr(),
+        predictions.data_ptr(),
+        costs.data_ptr(),
+        alphas.data_ptr(),
+        log_norms.data_ptr(),
+        labels.data_ptr(),
+        input_lengths.data_ptr(),
+        label_lengths.data_ptr(),
+        B, T, U, V, blank, is_cuda)
+    ctx.save_for_backward(
+        emissions, predictions, alphas, log_norms,
+        labels, input_lengths, label_lengths)
+    ctx.blank = blank
+    return costs
 
-    @staticmethod
-    def backward(ctx, cost):
-        is_cuda = cost.is_cuda
-        device = cost.device
-        dtype = cost.dtype
-        emissions, predictions, alphas, log_norms, labels, input_lengths, label_lengths = ctx.saved_tensors
-        B, T, V = emissions.shape
-        U = predictions.shape[1]
-        egrads = torch.empty(size=(B, T, V), device=device, dtype=dtype)
-        pgrads = torch.empty(size=(B, U, V), device=device, dtype=dtype)
-        _transducer.backward(
-            emissions.data_ptr(),
-            predictions.data_ptr(),
-            egrads.data_ptr(),
-            pgrads.data_ptr(),
-            alphas.data_ptr(),
-            log_norms.data_ptr(),
-            labels.data_ptr(),
-            input_lengths.data_ptr(),
-            label_lengths.data_ptr(),
-            B, T, U, V, ctx.blank, is_cuda)
-        return egrads, pgrads, None, None, None, None
+  @staticmethod
+  def backward(ctx, cost):
+    is_cuda = cost.is_cuda
+    device = cost.device
+    dtype = cost.dtype
+    emissions, predictions, alphas, log_norms, labels, input_lengths, label_lengths = ctx.saved_tensors
+    B, T, V = emissions.shape
+    U = predictions.shape[1]
+    egrads = torch.empty(size=(B, T, V), device=device, dtype=dtype)
+    pgrads = torch.empty(size=(B, U, V), device=device, dtype=dtype)
+    _transducer.backward(
+        emissions.data_ptr(),
+        predictions.data_ptr(),
+        egrads.data_ptr(),
+        pgrads.data_ptr(),
+        alphas.data_ptr(),
+        log_norms.data_ptr(),
+        labels.data_ptr(),
+        input_lengths.data_ptr(),
+        label_lengths.data_ptr(),
+        B, T, U, V, ctx.blank, is_cuda)
+    return egrads, pgrads, None, None, None, None
 
 
 class TransducerLoss(torch.nn.Module):
-    def __init__(self, blank=0):
-        super(TransducerLoss, self).__init__()
-        self.blank = blank
+  """
+  The RNN-T loss function.
 
-    def forward(self, emissions, predictions, labels, input_lengths, label_lengths):
-        return Transducer.apply(
-            emissions, predictions, labels, input_lengths, label_lengths, self.blank)
+  The loss can run on either the CPU ar the GPU based on the location of the
+  input tensors. All input tensors must be on the same device.
+
+  Arguments:
+      blank (int, optional): Integer id of blank label (default is 0).
+  """
+
+  def __init__(self, blank=0):
+    super(TransducerLoss, self).__init__()
+    self.blank = blank
+
+  def forward(self, emissions, predictions, labels, input_lengths, label_lengths):
+    """
+    Arguments:
+      emissions (FloatTensor): 3D tensor containing unnormalized emission
+        scores with shape (minibatch, input length, vocab size).
+      predictions (FloatTensor): 3D tensor containing unnormalized prediction
+        scores with shape (minibatch, output length + 1, vocab size).
+      labels (IntTensor): 2D tensor of labels for each example of shape
+        (minibatch, output length). Shorter labels should be padded to the
+        length of the longest label.
+      input_lengths (IntTensor): 1D tensor containing the input lengths of
+        each example.
+      label_lengths (IntTensor): 1D tensor containing the label lengths of
+        each example.
+
+    Returns:
+      costs (FloatTensor): 1D tensor with shape (minibatch) containing the
+        scores for each example in the batch.
+    """
+    return Transducer.apply(
+        emissions, predictions, labels, input_lengths, label_lengths, self.blank)
 
 
 def check_type(var, t, name):
-    if var.dtype is not t:
-        raise TypeError("{} must be {}".format(name, t))
+  if var.dtype is not t:
+    raise TypeError("{} must be {}".format(name, t))
 
 
 def check_contiguous(var, name):
-    if not var.is_contiguous():
-        raise ValueError("{} must be contiguous".format(name))
+  if not var.is_contiguous():
+    raise ValueError("{} must be contiguous".format(name))
 
 
 def check_dim(var, dim, name):
-    if len(var.shape) != dim:
-        raise ValueError("{} must be {}D".format(name, dim))
+  if len(var.shape) != dim:
+    raise ValueError("{} must be {}D".format(name, dim))
 
 
 def certify_inputs(emissions, predictions, labels, input_lengths, label_lengths):
-    check_type(emissions, torch.float32, "emissions")
-    check_type(predictions, torch.float32, "predictions")
-    check_type(labels, torch.int32, "labels")
-    check_type(input_lengths, torch.int32, "input_lengths")
-    check_type(label_lengths, torch.int32, "label_lengths")
-    check_contiguous(labels, "labels")
-    check_contiguous(label_lengths, "label_lengths")
-    check_contiguous(input_lengths, "lengths")
+  check_type(emissions, torch.float32, "emissions")
+  check_type(predictions, torch.float32, "predictions")
+  check_type(labels, torch.int32, "labels")
+  check_type(input_lengths, torch.int32, "input_lengths")
+  check_type(label_lengths, torch.int32, "label_lengths")
+  check_contiguous(labels, "labels")
+  check_contiguous(label_lengths, "label_lengths")
+  check_contiguous(input_lengths, "lengths")
 
-    batchSize = emissions.shape[0]
-    if emissions.shape[2] != predictions.shape[2]:
-        raise ValueError("vocab size mismatch.")
-    if input_lengths.shape[0] != batchSize:
-        raise ValueError("must have a length per example.")
-    if label_lengths.shape[0] != batchSize:
-        raise ValueError("must have a label length per example.")
-    if labels.shape[0] != batchSize:
-        raise ValueError("must have a label per example.")
-    if labels.shape[1] != (predictions.shape[1] - 1):
-        raise ValueError("labels must be padded to maximum label length.")
+  batchSize = emissions.shape[0]
+  if emissions.shape[2] != predictions.shape[2]:
+    raise ValueError("vocab size mismatch.")
+  if input_lengths.shape[0] != batchSize:
+    raise ValueError("must have a length per example.")
+  if label_lengths.shape[0] != batchSize:
+    raise ValueError("must have a label length per example.")
+  if labels.shape[0] != batchSize:
+    raise ValueError("must have a label per example.")
+  if labels.shape[1] != (predictions.shape[1] - 1):
+    raise ValueError("labels must be padded to maximum label length.")
 
-    check_dim(emissions, 3, "emissions")
-    check_dim(predictions, 3, "predictions")
-    check_dim(labels, 2, "labels")
-    check_dim(input_lengths, 1, "input_lenghts")
-    check_dim(label_lengths, 1, "label_lenghts")
-    max_T = torch.max(input_lengths)
-    max_U = torch.max(label_lengths)
-    T = emissions.shape[1]
-    U = predictions.shape[1]
-    if T != max_T:
-        raise ValueError("Input length mismatch")
-    if U != max_U + 1:
-        raise ValueError("Output length mismatch")
+  check_dim(emissions, 3, "emissions")
+  check_dim(predictions, 3, "predictions")
+  check_dim(labels, 2, "labels")
+  check_dim(input_lengths, 1, "input_lenghts")
+  check_dim(label_lengths, 1, "label_lenghts")
+  max_T = torch.max(input_lengths)
+  max_U = torch.max(label_lengths)
+  T = emissions.shape[1]
+  U = predictions.shape[1]
+  if T != max_T:
+    raise ValueError("Input length mismatch")
+  if U != max_U + 1:
+    raise ValueError("Output length mismatch")
